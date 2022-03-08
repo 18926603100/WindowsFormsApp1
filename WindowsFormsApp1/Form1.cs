fc8f374328cee5b6c1b27e46c98d5671c0b5b5d6
l q <184288021@qq.com>
2022-03-08 22:47:16 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("123");
+        }
+
+    
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) //keypress 按下某键 
+        {
+            if (e.KeyChar == (char)Keys.Enter)//按下的是否回车键
+            {
+                //button1.PerformClick(); //按下button1
+                button1_Click(null,null); //调用按下button1函数
+                 e.Handled = true;               //加上这个，没有 "咚" 的一声响
+            }
+        }
+    }
+}
